e761a850ef9f04ba83fdf3a2936406cc5a7fc6ed
kusum <kusumtilgam92@gmail.com>
2021-09-11 22:38:21 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from 'react';
+
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      
-      <h1>Hi, Kusum</h1>
-    </div>
-  );
+   <Switch>
+<Route exact path="/">
+  This is home page
+</Route>
+
+<Route exact path="/second">
+  This is second page
+</Route>
+
+<Route>
+  This is 404 page
+</Route>
+
+    </Switch>);
+
 }
 
 export default App;
