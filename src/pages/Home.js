155ac2a9af4d16c65e08cd5ba3d16db78fa1f4bf
kusum <kusumtilgam92@gmail.com>
2021-09-12 17:38:36 +0530

    displayed show result and small fetch refactor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,63 @@
-import React, {useState} from 'react'
-import MainPageLayout from '../components/MainPageLayout'
+
+
+
+
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+import { apiGet} from '../misc/config';
 
 const Home = () => {
-const [input, setInput] = useState('');
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
-const onInputChange = ev =>{
+  const onSearch = () => {
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
+  };
+
+  const onInputChange = ev => {
     setInput(ev.target.value);
-}
+  };
 
-
-const onSearch =() =>{
-   // https://api.tvmaze.com/search/shows?q=girls
-   fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-   .then(r => r.json())
-   .then(result => {
-       console.log(result);
-   });
-}
-
-const onKeyDown = ev =>{
-    if(ev.keyCode === 13){
-        onSearch();
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
     }
-   }
-    return (
-        <MainPageLayout>
-           <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
-           <button type="button" onClick={onSearch} >Search</button>
-        </MainPageLayout>
-    )
-}
+  };
 
-export default Home
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
+    </MainPageLayout>
+  );
+};
+
+export default Home;
